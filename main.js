055bb2c827988c158b22f0535016a2e2a6b98f5c
donzo93 <10548617@polimi.it>
2021-08-21 14:55:24 +0200

    commented everything

--- main.js
+++ main.js
@@ -71,91 +71,91 @@ function main() {
 
   var mesh = new OBJ.Mesh(hammer);
 
-  //use this aspect ratio to keep proportions
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  gl.clearColor(0, 0, 0, 0);
+  // //use this aspect ratio to keep proportions
+  // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  // gl.clearColor(0, 0, 0, 0);
 
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.enable(gl.CULL_FACE);
-  gl.enable(gl.DEPTH_TEST);
+  // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  // gl.enable(gl.CULL_FACE);
+  // gl.enable(gl.DEPTH_TEST);
 
-  // create GLSL shaders, upload the GLSL source, compile the shaders and link them
-  var vertexShader = utils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  var fragmentShader = utils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-  var program = utils.createProgram(gl, vertexShader, fragmentShader);
+  // // create GLSL shaders, upload the GLSL source, compile the shaders and link them
+  // var vertexShader = utils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  // var fragmentShader = utils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  // var program = utils.createProgram(gl, vertexShader, fragmentShader);
 
-  // look up where the vertex data needs to go.
-  //var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  var colorAttributeLocation = gl.getAttribLocation(program, "a_color");
-  matrixLocation = gl.getUniformLocation(program, "matrix");
+  // // look up where the vertex data needs to go.
+  // //var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  // var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  // var colorAttributeLocation = gl.getAttribLocation(program, "a_color");
+  // matrixLocation = gl.getUniformLocation(program, "matrix");
 
 
-  perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width / gl.canvas.height, 0.1, 100.0);
+  // perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width / gl.canvas.height, 0.1, 100.0);
 
-  // Create a vertex array object (attribute state)
-  vao = gl.createVertexArray();
-  // and make it the one we're currently working with
-  gl.bindVertexArray(vao);
-  // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  // // Create a vertex array object (attribute state)
+  // vao = gl.createVertexArray();
+  // // and make it the one we're currently working with
+  // gl.bindVertexArray(vao);
+  // // Create a buffer and put three 2d clip space points in it
+  // var positionBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
+  // gl.enableVertexAttribArray(positionAttributeLocation);
+  // gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 
-  var colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(colorAttributeLocation);
-  gl.vertexAttribPointer(colorAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  // var colorBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
+  // gl.enableVertexAttribArray(colorAttributeLocation);
+  // gl.vertexAttribPointer(colorAttributeLocation, 3, gl.FLOAT, false, 0, 0);
 
-  var indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(mesh.indices), gl.STATIC_DRAW);
+  // var indexBuffer = gl.createBuffer();
+  // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(mesh.indices), gl.STATIC_DRAW);
   
   
-  drawScene();
+  // drawScene();
 
-  function animate() {
-    var currentTime = (new Date).getTime();
-    if (lastUpdateTime) {
-      //currentTime – lastUpdateTime is the time passed between frames
-      var deltaC = (3 * (currentTime - lastUpdateTime)) / 1000.0;
-      if (flag == 0) cubeTx += deltaC;
-      else cubeTx -= deltaC;
-      if (cubeTx >= 1.5) flag = 1;
-      else if (cubeTx <= -1.5) flag = 0;
-    }
-    worldMatrix = utils.MakeWorld(cubeTx, cubeTy, cubeTz, cubeRx, cubeRy, cubeRz, cubeS);
-    lastUpdateTime = currentTime; //Need to update it for the next frame
-  }
+  // function animate() {
+  //   var currentTime = (new Date).getTime();
+  //   if (lastUpdateTime) {
+  //     //currentTime – lastUpdateTime is the time passed between frames
+  //     var deltaC = (3 * (currentTime - lastUpdateTime)) / 1000.0;
+  //     if (flag == 0) cubeTx += deltaC;
+  //     else cubeTx -= deltaC;
+  //     if (cubeTx >= 1.5) flag = 1;
+  //     else if (cubeTx <= -1.5) flag = 0;
+  //   }
+  //   worldMatrix = utils.MakeWorld(cubeTx, cubeTy, cubeTz, cubeRx, cubeRy, cubeRz, cubeS);
+  //   lastUpdateTime = currentTime; //Need to update it for the next frame
+  // }
 
 
-  function drawScene() {
-    animate();
+  // function drawScene() {
+  //   animate();
 
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
-    gl.useProgram(program);
+  //   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  //   gl.clearColor(0, 0, 0, 0);
+  //   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  //   gl.enable(gl.DEPTH_TEST);
+  //   gl.enable(gl.CULL_FACE);
+  //   gl.useProgram(program);
 
-    // Bind the attribute/buffer set we want.
-    gl.bindVertexArray(vao);
+  //   // Bind the attribute/buffer set we want.
+  //   gl.bindVertexArray(vao);
 
-    var viewMatrix = utils.MakeView(cx, cy, cz, elevation, angle);
+  //   var viewMatrix = utils.MakeView(cx, cy, cz, elevation, angle);
 
-    var projectionMatrix = utils.multiplyMatrices(viewMatrix, worldMatrix);
-    var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, projectionMatrix);
+  //   var projectionMatrix = utils.multiplyMatrices(viewMatrix, worldMatrix);
+  //   var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, projectionMatrix);
 
-    gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix));
+  //   gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix));
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.drawElements(gl.TRIANGLES, mesh.indices.length, gl.UNSIGNED_SHORT, 0);
-    window.requestAnimationFrame(drawScene);
-  }
+  //   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  //   gl.drawElements(gl.TRIANGLES, mesh.indices.length, gl.UNSIGNED_SHORT, 0);
+  //   window.requestAnimationFrame(drawScene);
+  // }
 
 }
 
