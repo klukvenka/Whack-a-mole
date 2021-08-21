d6e157ce2a1c322132319d47ddc65d60fec7da7f
donzo93 <10548617@polimi.it>
2021-08-21 17:01:38 +0200

    commented code

--- main.js
+++ main.js
@@ -52,12 +52,6 @@ var angle = -30.0;
 var delta = 0.1;
 var flag = 0;
 
-/*async function initMesh () {
-  let objStr = await utils.get_objstr("Assets/hammer.obj");
-
-  return new OBJ.Mesh(objStr);  
-};*/
-
 function main() {
 
   // Get a WebGL context
@@ -69,9 +63,9 @@ function main() {
   }
   utils.resizeCanvasToDisplaySize(gl.canvas);
 
+  //Loads object mesh from assets
   var mesh = new OBJ.Mesh(hammer);
   
-
   //use this aspect ratio to keep proportions
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.clearColor(0, 0, 0, 0);
@@ -80,10 +74,12 @@ function main() {
   gl.enable(gl.CULL_FACE);
   gl.enable(gl.DEPTH_TEST);
 
+  //creates, compiles and link shaders to gl program
   programInfo = twgl.createProgramInfo(gl, [vertexShaderSource, fragmentShaderSource]);
   console.log(mesh.indices);
 
 
+  //creating an object containing vertices, normals, idexes etc. to use with twgl
   const bufferInfo = twgl.createBufferInfoFromArrays(gl, {
     a_position: mesh.vertices,
     color: mesh.vertices,
@@ -91,44 +87,13 @@ function main() {
     }
     );
 
-  // // create GLSL shaders, upload the GLSL source, compile the shaders and link them
-  // var vertexShader = utils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  // var fragmentShader = utils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-  // var program = utils.createProgram(gl, vertexShader, fragmentShader);
-
-  // // look up where the vertex data needs to go.
-  // //var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  // var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  // var colorAttributeLocation = gl.getAttribLocation(program, "a_color");
-  // matrixLocation = gl.getUniformLocation(program, "matrix");
-
+  //creating an object containing all uniforms
   const uniforms = {
     matrix: []
   };
 
+  //creating perspective matrix
   perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width / gl.canvas.height, 0.1, 100.0);
-
-  // // Create a vertex array object (attribute state)
-  // vao = gl.createVertexArray();
-  // // and make it the one we're currently working with
-  // gl.bindVertexArray(vao);
-  // // Create a buffer and put three 2d clip space points in it
-  // var positionBuffer = gl.createBuffer();
-  // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
-  // gl.enableVertexAttribArray(positionAttributeLocation);
-  // gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-
-  // var colorBuffer = gl.createBuffer();
-  // gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
-  // gl.enableVertexAttribArray(colorAttributeLocation);
-  // gl.vertexAttribPointer(colorAttributeLocation, 3, gl.FLOAT, false, 0, 0);
-
-  // var indexBuffer = gl.createBuffer();
-  // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(mesh.indices), gl.STATIC_DRAW);
-  
   
   drawScene();
 
@@ -156,25 +121,27 @@ function main() {
     gl.enable(gl.CULL_FACE);
     gl.useProgram(programInfo.program);
 
-    // // Bind the attribute/buffer set we want.
-    // gl.bindVertexArray(vao);
+    //creating projection matrix
     var worldMatrix = utils.MakeWorld(cubeTx, cubeTy, cubeTz, cubeRx, cubeRy, cubeRz, cubeS);
     var viewMatrix = utils.MakeView(cx, cy, cz, elevation, angle);
 
     var projectionMatrix = utils.multiplyMatrices(viewMatrix, worldMatrix);
     var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, projectionMatrix);
 
-    // gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix));
-
-    // uniforms.u_viewInverse = camera;
-    // uniforms.u_world = world;
-    // uniforms.u_worldInverseTranspose = m4.transpose(m4.inverse(world));
+    //populating uniform object with projmatrix (the only uniform we use in this program)
     uniforms.matrix = utils.transposeMatrix(projectionMatrix);
 
+    //binding buffers and attributes to program
     twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+    
+    //binding uniforms to program
     twgl.setUniforms(programInfo, uniforms);
-    gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
+    
+    //draw the elements!
+    // gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0);
+    twgl.drawBufferInfo(gl, bufferInfo);
 
+    //continuously recalls himself
     window.requestAnimationFrame(drawScene);
 
   }
